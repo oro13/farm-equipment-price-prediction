ba70e3bdf98b83fed1fe651e037d63d441284d0c
boogiedev <nguyenw@seattleu.edu>
2020-04-28 12:28:01 -0700

    gradient clean

--- src/gradient_clean_data.py
+++ src/gradient_clean_data.py
@@ -4,7 +4,7 @@ import us
 import re
 from src.helpers import *
 
-def gradient_clean_df(df:pd.DataFrame) -> tuple:
+def gradient_clean_Xy(df:pd.DataFrame) -> tuple:
     df_cop = df.copy()
     # Drop Columns
     df_cop.drop(columns=['MachineID', 
@@ -13,18 +13,19 @@ def gradient_clean_df(df:pd.DataFrame) -> tuple:
     # Create "Vehicle Type" Feature from "fiProductClassDesc"
     df_cop["Vehicle Type"] = df_cop["ProductGroupDesc"]
     df_cop["Power Rating"] = df_cop["fiProductClassDesc"].apply(lambda x:(x.partition("-")[-1]))
-    df_cop.drop(columns='fiProductClassDesc', inplace=True)
+    df_cop.drop(columns ='fiProductClassDesc', inplace=True)
     
     # Make a horsepower column
     df_hp = df_cop[df_cop["Power Rating"].str.contains('horsepower', case=False)]
-    df_cop['HorsePower'] = df_hp['Power Rating'].map(getReMax)
+    df_cop['Horsepower'] = df_hp['Power Rating'].map(getReMax)
     
     # Convert state names to state numbers
-    us_dict = us.states.mapping('name', 'fips')
-    us_dict["Washington DC"] = 3
-    print(us_dict)
-    us_dict["Unspecified"] = '0'
-    df_cop["state"]=df_cop["state"].map(lambda x: int(us_dict[x.strip()]))
+    us_dict = us.states.mapping('name', 'abbr')
+    us_dict["Washington DC"] = 'DC'
+    us_dict["Unspecified"] = 'None'
+    df_cop["state"] = df_cop["state"].map(lambda x: us_dict[x.strip()])
     
+    X = df_cop
+    y = df_cop.pop("SalePrice")
     
-    return df
+    return X, y
