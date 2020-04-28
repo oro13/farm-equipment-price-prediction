eccb04c60810c0a9fc69bce158cdace0fcc75a64
boogiedev <nguyenw@seattleu.edu>
2020-04-28 12:02:04 -0700

    gradient model

--- src/gradient_clean_data.py
+++ src/gradient_clean_data.py
@@ -5,24 +5,26 @@ import re
 from src.helpers import *
 
 def gradient_clean_df(df:pd.DataFrame) -> tuple:
-    
-#     # Drop Columns
-#     df.drop(columns=['UsageBand','Blade_Extension', 'Blade_Width', 'Enclosure_Type',
-#                      'Engine_Horsepower', 'Pushblock', 'Scarifier', 'Tip_Control',
-#                      'Coupler_System', 'Grouser_Tracks', 'Hydraulics_Flow','Backhoe_Mounting', 
-#                      'Blade_Type', 'Travel_Controls','Differential_Type','Steering_Controls',
-#                      'SalesID','fiBaseModel','fiSecondaryDesc',
-#                      'fiModelSeries','fiModelDescriptor', 'auctioneerID',
-#                      'Drive_System', 'datasource'
-#                     ], inplace=True)
+    df_cop = df.copy()
+    # Drop Columns
+    df_cop.drop(columns=['MachineID', 
+                    ], inplace=True)
     
     # Create "Vehicle Type" Feature from "fiProductClassDesc"
-    df["Vehicle Type"] = df["ProductGroupDesc"]
-    df["Power Rating"] = df["fiProductClassDesc"].apply(lambda x:(x.partition("-")[-1]))
-    df.drop(columns='fiProductClassDesc', inplace=True)
+    df_cop["Vehicle Type"] = df_cop["ProductGroupDesc"]
+    df_cop["Power Rating"] = df_cop["fiProductClassDesc"].apply(lambda x:(x.partition("-")[-1]))
+    df_cop.drop(columns='fiProductClassDesc', inplace=True)
     
     # Make a horsepower column
-    df_hp = df[df["Power Rating"].str.contains('horsepower', case=False)]
-    df['HorsePower'] = df_hp['Power Rating'].map(getReMax)
+    df_hp = df_cop[df_cop["Power Rating"].str.contains('horsepower', case=False)]
+    df_cop['HorsePower'] = df_hp['Power Rating'].map(getReMax)
+    
+    # Convert state names to state numbers
+    us_dict = us.states.mapping('name', 'fips')
+    us_dict["Washington DC"] = 3
+    print(us_dict)
+    us_dict["Unspecified"] = '0'
+    df_cop["state"]=df_cop["state"].map(lambda x: int(us_dict[x.strip()]))
+    
     
     return df
