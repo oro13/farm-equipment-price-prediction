477cf3933f085399d8760564f4d17b84b5b4aa38
boogiedev <nguyenw@seattleu.edu>
2020-04-28 18:05:28 -0700

    further GB cleaning

--- src/generate_report.py
+++ src/generate_report.py
@@ -20,17 +20,23 @@ Error occured, check csv file path in "generate_report.py"
 
 if __name__ == '__main__':
     try:
+        # Get path and filename from user
+        path = input("Enter path for csv file: ")
+        path = "../data/Train.csv" if path.strip() == "" else path
+        file_name = input("Enter file name for csv file: ")
+        file_name = "profile_report.html" if file_name.strip() == "" else f"{file_name}.html"
+        
         # Create Dataframe
         print(success[0])
-        df = pd.read_csv("../data/Train.csv")
+        df = pd.read_csv(path)
         
         # Generate Profile
         print(success[1])
         profile = ProfileReport(df, title='Profiling Report', html={'style':{'full_width':True}})
-        
+
         # Output Profile as HTML in Local Dir
         print(success[2])
-        profile.to_file(output_file="profile_report.html")
+        profile.to_file(output_file=file_name)
 
         # Validate Success
         print(success[3])
--- src/gradient_clean_data.py
+++ src/gradient_clean_data.py
@@ -2,22 +2,23 @@ import pandas as pd
 import numpy as np
 import us
 import re
+from sklearn.preprocessing import LabelEncoder
 from src.helpers import *
 
 def gradient_clean_Xy(df:pd.DataFrame) -> tuple:
+    # Copy Dataframe
     df_cop = df.copy()
-    # Drop Columns
-    df_cop.drop(columns=['MachineID', 
-                    ], inplace=True)
+    
+    
+    'CREATE/CONVERT COLUMNS'
     
     # Create "Vehicle Type" Feature from "fiProductClassDesc"
-    df_cop["Vehicle Type"] = df_cop["ProductGroupDesc"]
-    df_cop["Power Rating"] = df_cop["fiProductClassDesc"].apply(lambda x:(x.partition("-")[-1]))
-    df_cop.drop(columns ='fiProductClassDesc', inplace=True)
+    df_cop["Vehicle_Type"] = df_cop["ProductGroupDesc"]
+    df_cop["Power_Rating"] = df_cop["fiProductClassDesc"].apply(lambda x:(x.partition("-")[-1]))
     
     # Make a horsepower column
-    df_hp = df_cop[df_cop["Power Rating"].str.contains('horsepower', case=False)]
-    df_cop['Horsepower'] = df_hp['Power Rating'].map(getReMax)
+    df_hp = df_cop[df_cop["Power_Rating"].str.contains('horsepower', case=False)]
+    df_cop['Horsepower'] = df_hp['Power_Rating'].map(getReMax)
     
     # Convert state names to state numbers
     us_dict = us.states.mapping('name', 'abbr')
@@ -25,6 +26,49 @@ def gradient_clean_Xy(df:pd.DataFrame) -> tuple:
     us_dict["Unspecified"] = 'None'
     df_cop["state"] = df_cop["state"].map(lambda x: us_dict[x.strip()])
     
+    # Convert "None or Unspecified rows into Unspecified"
+    df_cop["Turbocharged"] = df_cop["Turbocharged"].map(lambda x: "Unspecified" if x != "Yes" else x)
+    # Convert NaN rows into Unspecified"
+    df_cop["UsageBand"] = df_cop["UsageBand"].map(lambda x: "Unspecified" if pd.isnull(x) else x)
+    df_cop["Engine_Horsepower"] = df_cop["Engine_Horsepower"].map(lambda x: "Unspecified" if pd.isnull(x) else x)
+    df_cop["Drive_System"] = df_cop["Drive_System"].map(lambda x: "Unspecified" if pd.isnull(x) else x)
+    df_cop["Enclosure"] = df_cop["Enclosure"].map(lambda x: "Unspecified" if (pd.isnull(x) or x == "None or Unspecified") else x)
+    df_cop["Forks"] = df_cop["Forks"].map(lambda x: "Unspecified" if (pd.isnull(x) or x == "None or Unspecified") else x)
+    df_cop["Pad_Type"] = df_cop["Pad_Type"].map(lambda x: "Unspecified" if (pd.isnull(x) or x == "None or Unspecified") else x)
+    df_cop["Ride_Control"] = df_cop["Ride_Control"].map(lambda x: "Unspecified" if (pd.isnull(x) or x == "None or Unspecified") else x)
+    
+    
+    # Modify Enclosure Feature for Consistency
+    # EROPS w AC == EROPS AC, NO ROPS == Unspecified
+    sub = {"EROPS w AC":"EROPS AC", "NO ROPS":"Unspecified"}
+    df_cop["Enclosure"] = df_cop["Enclosure"].map(lambda x: sub[x] if (x in sub) else x)
+    
+    
+    
+    
+    # Convert SalesDate to datetime object
+    df_cop["saledate"] = pd.to_datetime(df_cop["saledate"])
+
+    
+    'ENCODE COLUMNS'
+    le = LabelEncoder()
+    # List of features to encode
+    features = ["UsageBand", "fiBaseModel", "fiSecondaryDesc", "fiModelDescriptor", "ProductSize", "state", "Vehicle Type", ]
+    
+    'DROP COLUMNS'
+
+    # Drop Unecessary Columns
+    df_cop.drop(columns=['MachineID', 'SalesID', 'auctioneerID', 
+                    ], inplace=True)
+    
+    # Drop Dupe Columns
+    df_cop.drop(columns =['fiProductClassDesc', 'ProductGroupDesc', 'ProductGroup', 'fiModelDesc', 'fiModelSeries'], inplace=True)
+    # Drop ModelDesc | Keep Base Model
+    
+    'RENAME COLUMNS FOR CLARITY'
+#     df_cop = df_cop.rename(columns={'fiModelDesc': 'Model Description'})
+    
+    # Return train/target split
     X = df_cop
     y = df_cop.pop("SalePrice")
     
